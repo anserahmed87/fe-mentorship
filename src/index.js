8d23bc08916b90ecda18f71804c18cac09931208
Anser Ahmed <Anser.Ahmed@nordnet.se>
2018-04-25 10:41:52 +0200

    updated packges

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+//export { * } from './page';
+//export { * } from './header';
+//export { * } from './content';
+//export { * } from './footer';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
