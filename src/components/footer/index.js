1ba4f9a85c1f9dcf04baa33297c8f764b30cb06c
Anser Ahmed <Anser.Ahmed@nordnet.se>
2018-04-26 16:19:42 +0200

    corrected header

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -7,7 +7,7 @@ const footerMenu = [
       { text: 'Documentation',    url: 'https://reactjs.org/docs/' },
       { text: 'PropTypes',        url: 'https://reactjs.org/docs/typechecking-with-proptypes.html' },
       { text: 'Create react app', url: 'https://github.com/facebook/create-react-app/' },
-      { text: 'github',           url: 'https://github.com/vahidabbasi/mentor-ship' },
+      { text: 'github',           url: 'https://github.com/anserahmed87/fe-mentorship' },
     ];
 
 export const Footer = () => (
