bae623b35823af31da398776a5bc36e6a1004502
Anser Ahmed <Anser.Ahmed@nordnet.se>
2018-04-26 17:13:00 +0200

    add page formatting

--- src/components/page/index.js
+++ src/components/page/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import { Grid, Row, Col } from '@nordnet/grid';
+import './styles.css';
 
 export const Page = ({ children }) => (
-    <Grid>
-        <Row>
-            <Col xs={12}>
-                { children }
-            </Col>
-        </Row>
-    </Grid>
-)
+    <div className="page">
+        { children }
+    </div>
+);
