5604a194f6b7c38ad12a5c58b875bde9b8cd055f
Anser Ahmed <Anser.Ahmed@nordnet.se>
2018-05-09 15:33:53 +0200

    make logo a Link

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { Menu } from '../';
+import {Menu} from '../';
+import {Link} from 'react-router-dom';
 import './styles.css';
 
 const mainMenu = [
-      { text: 'New',    url: 'https://news.ycombinator.com/newest' },
-      { text: 'Show',   url: 'https://news.ycombinator.com/show' },
-      { text: 'Submit', url: 'https://news.ycombinator.com/submit' },
-    ];
+    {text: 'New', url: 'https://news.ycombinator.com/newest'},
+    {text: 'Show', url: 'https://news.ycombinator.com/show'},
+    {text: 'Submit', url: 'https://news.ycombinator.com/submit'},
+];
 
 export const Header = () => (
     <div className="header">
-        <span className="header_logo">
-         <img src="https://news.ycombinator.com/y18.gif" alt= "React new logo" />
-            news here
-        </span>
+        <Link to={`/`}>
+            <span className="header_logo">
+                 <img src="https://news.ycombinator.com/y18.gif" alt="React new logo"/>
+            </span>
+        </Link>
         <div className="header_menu">
             <Menu links={mainMenu}/>
         </div>
