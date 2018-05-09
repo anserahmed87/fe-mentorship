88d792c2eac820107c15b827c1e83bce3d883e6a
Anser Ahmed <Anser.Ahmed@nordnet.se>
2018-05-09 10:31:05 +0200

    wrap App contents into Router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, {Component} from 'react';
+import {BrowserRouter as Router} from 'react-router-dom'
 
 import {Page, Header, Footer, Content} from './components';
 
 class App extends Component {
     render() {
         return (
+            <Router>
             <Page>
                 <div>
                     <Header/>
-                    <Content/>
+                    <Content>
+                        This is content
+                    </Content>
                 </div>
                 <Footer/>
             </Page>
+            </Router>
         );
     }
 }
