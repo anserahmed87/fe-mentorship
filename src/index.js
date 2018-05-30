73cbc09d533997b0de946bc8e82a14eb54c09106
Anser Ahmed <Anser.Ahmed@nordnet.se>
2018-05-30 13:44:54 +0200

    Fixed store to show redux

--- src/index.js
+++ src/index.js
@@ -8,7 +8,10 @@ import { createStore, } from 'redux';
 import { Provider } from 'react-redux';
 
 
-const store = createStore(rootReducer);
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
     <Provider store={store}>
